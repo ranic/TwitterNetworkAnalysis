3ae190c54ec38b7b1060b3ac0afd0341c82b4908
vijay <vijay.iyengar713@gmail.com>
2015-03-26 16:02:05 -0400

    Even partitioning of tweets by time, with pruning

--- generate_nodes_by_time.py
+++ generate_nodes_by_time.py
@@ -10,8 +10,12 @@ from Tweet import Tweet
 import json
 
 TIME_FORMAT = "%a %b %d %H:%M:%S  %Y"
-SIMPLE_DATE_FORMAT = "%m-%d-%y"
 TWEETS_BY_HASHTAG = "hashtags/%s.out"
+
+NUM_WINDOWS = 10
+# Percent of initial outliers to prune
+START_PRUNE = 0.05
+END_PRUNE = 0.8
 
 with open(sys.argv[1], 'r') as f:
     hashtags = map(lambda x: x[:-1], f.readlines())
@@ -21,50 +25,42 @@ for hashtag in hashtags:
     with open("hashtags/%s.out" % hashtag, "rb") as f:
         tweets = pickle.load(f)
 
-    # Users that tweeted hashtag before FIRST_DATE
-    first_users = set()
-    # Before SECOND_DATE 
-    second_users = set()
-    # Before THIRD_DATE 
-    third_users = set()
+    # Partitions of users based on timestamp of tweet
+    partitions = [set() for _ in xrange(NUM_WINDOWS)]
 
-    # Set date boundaries
-    times = [time.strptime(t.time, TIME_FORMAT) for t in tweets]
-    times.sort()
+    # Set date boundaries (TODO: optimize this out)
+    times = sorted([time.strptime(t.time, TIME_FORMAT) for t in tweets])
+    (start_index, end_index) = len(times)*START_PRUNE, len(times)*END_PRUNE
+    times = times[int(start_index):int(end_index)]
 
-    FIRST_DATE = times[len(times)/3]
-    SECOND_DATE = times[2*len(times)/3]
-    THIRD_DATE = times[-1]
+    # Determine size of time window for each partition
+    start_time = time.mktime(times[0])
+    end_time = time.mktime(times[-1])
+    time_width = end_time - start_time + 1
+    window_size = time_width / NUM_WINDOWS
 
-    # Collect all users mentioned in relation to this hashtag
+    # Collect all users mentioned in relation to this hashtag and partition by time
     for t in tweets:
-        timestamp = time.strptime(t.time, TIME_FORMAT)
+        # TODO: Do filtering to optimize this
+        # Determine partition for this tweet
+        timestamp = time.mktime(time.strptime(t.time, TIME_FORMAT))
+        delta = timestamp - start_time
+        if (timestamp < start_time or timestamp > end_time): continue
+        index = int(delta / window_size)
 
-        if (timestamp > SECOND_DATE):
-            third_users.update(t.mentioned_entities)
-        elif (timestamp > FIRST_DATE):
-            second_users.update(t.mentioned_entities)
-        else:
-            first_users.update(t.mentioned_entities)
-
-    # Make the sets cumulative
-    second_users.update(first_users)
-    third_users.update(second_users)
+        # Add nodes to that partition
+        partitions[index].update(t.mentioned_entities)
 
     if not os.path.exists("graphs/%s" % hashtag):
        os.makedirs("graphs/%s" % hashtag)
 
     with open('graphs/%s/times.csv' % hashtag, 'w') as f:
-        json.dump(map(lambda x: time.strftime(SIMPLE_DATE_FORMAT, x), [FIRST_DATE, SECOND_DATE, THIRD_DATE]), f)
+        # Store the start of each partition, to be used as time labels in visualizations
+        json.dump([(start_time + i*window_size) for i in xrange(NUM_WINDOWS)], f)
 
     # Write out user (node) lists
-    with open('graphs/%s/nodes1.csv' % hashtag, 'w') as f:
-        f.write("\n".join(sorted(first_users)))
-
-    with open('graphs/%s/nodes2.csv' % hashtag, 'w') as f:
-        f.write("\n".join(sorted(second_users)))
-
-    with open('graphs/%s/nodes3.csv' % hashtag, 'w') as f:
-        f.write("\n".join(sorted(third_users)))
+    for i in xrange(len(partitions)):
+        with open('graphs/%s/nodes%d.csv' % (hashtag, i+1), 'w') as f:
+            f.write("\n".join(sorted(partitions[i])))
 
     print "Wrote out node lists for ", hashtag
